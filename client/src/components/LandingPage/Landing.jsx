d27dd048aa0fef28bf4fcdd89d4db92b6e832984
Fabiola <fabiola@fabi>
2021-07-17 19:09:36 -0300

    un poco de css

--- client/src/components/LandingPage/Landing.jsx
+++ client/src/components/LandingPage/Landing.jsx
@@ -5,41 +5,11 @@ import style from './Landing.module.css'
 export default function LandingPage() {
     return (
         <div className={style.content} >
-            {/* <img className={style.myImg} src="https://images.unsplash.com/photo-1612404459571-ccef4b6588e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=751&q=80" alt="Not found"/> */}
-            <h1>Welcome to Favi-Con!</h1>
-            <p>This a Videogame-Com, you can find all videogames that you like or discover a new one...</p>
+            <div className={style.conTitle}>
+                <h1 className={style.title}>Welcome to Favi-Con!</h1>
+                <h1 className={style.title}>a web page where you can find videogames</h1>
+            </div>
             <Link to='/videogames'><button className={style.bt}>Press to start</button></Link>
         </div>
     )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export default function LandingPage() {
-//     return (
-//         <div className={style.background}>
-//             <h1>Welcome to Favi-Con!</h1>
-//             <h2>algo</h2>
-//             <Link to='/videogames'><button className={style.bt}>Press to start</button></Link>
-//         </div>
-//     )
-// }
