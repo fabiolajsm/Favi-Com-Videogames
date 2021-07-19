da76d396e3cf8d1b7dffeca157f4418c1a9c70dc
Fabiola <fabiola@fabi>
2021-07-19 10:55:48 -0300

    A-commit

--- client/src/actions/getVideogames.js
+++ client/src/actions/getVideogames.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
 import { GET_VIDEOGAMES } from './constants'
 
-
 export const games = (videogames) => ({
     type: GET_VIDEOGAMES,
     payload: videogames
-})
+});
 
 export const getAllGames = () => {
     return (dispatch) => {
         return axios.get(`http://localhost:3001/videogames`)
             .then(videogames => {
-                // console.log(videogames, 'videogameeeeeeeessssssssss');
                 dispatch(games(videogames.data))
             })
     }
-}
+};
